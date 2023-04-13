1979d715a580d6b5c726984ece787c2c1d842cda
shixuewen <friendlysxw@163.com>
2023-04-13 09:35:38 +0800

    fix: export profile components

--- packages/profile/src/index.ts
+++ packages/profile/src/index.ts
@@ -1,1 +1,13 @@
 export { default as S3ProfileModel } from "./data-model";
+
+export * from "./components";
+
+export {
+  useProfileState,
+  useProfileForDid,
+} from "./components/ProfileProvider/ProfileStateContext";
+export {
+  useAuthentication,
+  useIsAuthenticated,
+  useSession,
+} from "./components/ProfileProvider/AuthenticationContext";
