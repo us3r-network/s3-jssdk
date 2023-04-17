2cf81637c3c544d1f51e4936fbb246f722dfbef8
ttang <ttang@ttang-MacBook-Pro-2021.local>
2023-04-17 15:38:03 +0800

    feat: linkModel fix

--- packages/link/src/data-model/comment.ts
+++ packages/link/src/data-model/comment.ts
@@ -1,6 +1,6 @@
 import { RuntimeCompositeDefinition } from "@composedb/types";
 import type { CeramicApi } from "@ceramicnetwork/common";
-import S3Model from "@us3r-network/data-model";
+import { S3Model } from "@us3r-network/data-model";
 import { Page } from "@ceramicnetwork/common";
 
 import { definition as linkDefinition } from "./link-runtime-composite";
--- packages/link/src/data-model/index.ts
+++ packages/link/src/data-model/index.ts
@@ -1,5 +1,14 @@
-export { S3LinkModel, Link, Creator, DateTime } from "./link";
-export { S3FavorModel, Favor, FavorInput } from "./favor";
-export { S3CommentModel, Comment, CommentInput } from "./comment";
-export { S3ScoreModel, Score, ScoreInput } from "./score";
-export { S3VoteModel, Vote, VoteInput, VoteType } from "./vote";
+export { S3LinkModel } from "./link";
+export type { Link, Creator, DateTime } from "./link";
+
+export { S3FavorModel } from "./favor";
+export type { Favor, FavorInput } from "./favor";
+
+export { S3CommentModel } from "./comment";
+export type { Comment, CommentInput } from "./comment";
+
+export { S3ScoreModel } from "./score";
+export type { Score, ScoreInput } from "./score";
+
+export { S3VoteModel } from "./vote";
+export type { Vote, VoteInput, VoteType } from "./vote";
