d433ea1610297958a8870fb5210e6f83c143ce08
shixuewen <friendlysxw@163.com>
2023-04-27 16:46:45 +0800

    build: update storybook

--- packages/profile/src/components/LogoutButton/LogoutButton.stories.ts
+++ packages/profile/src/components/LogoutButton/LogoutButton.stories.ts
@@ -3,7 +3,6 @@ import { LogoutButton as LogoutButtonComponent } from ".";
 const meta = {
   title: "Components/LogoutButton",
   component: LogoutButtonComponent,
-  tags: ["autodocs"],
 } satisfies Meta<typeof LogoutButtonComponent>;
 export default meta;
 
--- packages/profile/src/components/UserAvatar/UserAvatar.stories.ts
+++ packages/profile/src/components/UserAvatar/UserAvatar.stories.ts
@@ -4,7 +4,6 @@ import { DEFAULT_DID } from "../../utils/constants";
 const meta = {
   title: "Components/UserAvatar",
   component: UserAvatarComponent,
-  tags: ["autodocs"],
 } satisfies Meta<typeof UserAvatarComponent>;
 export default meta;
 
@@ -13,8 +12,8 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const LoginUserAvatar: Story = {};
 
-export const OtherUserAvatar: Story = {
-  args: {
-    did: DEFAULT_DID,
-  },
-};
+// export const OtherUserAvatar: Story = {
+//   args: {
+//     did: DEFAULT_DID,
+//   },
+// };
