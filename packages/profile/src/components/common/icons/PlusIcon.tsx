85d3bb94d54ae3f83c123cbc903c03761c909e79
shixuewen <friendlysxw@163.com>
2023-05-06 11:28:12 +0800

    fix: google icon

--- packages/profile/src/components/common/icons/PlusIcon.tsx
+++ packages/profile/src/components/common/icons/PlusIcon.tsx
@@ -1,19 +1,5 @@
+import { checkCreateMaterialSymbolsLinkToHead } from "../../../utils/materialSymbols";
 export default function PlusIcon() {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M12 5V19M5 12H19"
-        stroke="#718096"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
+  checkCreateMaterialSymbolsLinkToHead();
+  return <span className="material-symbols-outlined">add</span>;
 }
