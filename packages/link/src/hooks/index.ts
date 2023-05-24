e98a48892f46ad4a232d46f422a55b9af6ef7496
shixuewen <friendlysxw@163.com>
2023-05-24 16:13:30 +0800

    fix: export hooks

--- packages/link/src/hooks/index.ts
+++ packages/link/src/hooks/index.ts
@@ -1,3 +1,13 @@
 export { useLink } from "./useLink";
-export { useFavorAction } from "./useFavorAction";
+
 export { useVoteAction } from "./useVoteAction";
+
+export { useCommentAction } from "./useCommentAction";
+export { useComments } from "./useComments";
+
+export { useFavorAction } from "./useFavorAction";
+export { usePersonalFavors } from "./usePersonalFavors";
+
+export { useScoreAction } from "./useScoreAction";
+export { useScores } from "./useScores";
+export { usePersonalScores } from "./usePersonalScores";
