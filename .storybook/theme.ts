eff0243e45e2679919a3d42ee7cdb86f7b455fcc
shixuewen <friendlysxw@163.com>
2023-05-31 11:44:48 +0800

    docs: storybook structure

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -2,7 +2,7 @@ import { create } from "@storybook/theming/create";
 
 export default create({
   base: "light",
-  brandTitle: "S3 Profile",
+  brandTitle: "Underglaze",
   brandUrl: "https://scan.s3.xyz/",
   brandImage: `https://scan.s3.xyz/favicon.ico`,
   brandTarget: "__blank",
