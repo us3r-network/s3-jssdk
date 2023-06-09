6efe21433f49bc802059723825b242207320c152
shixuewen <friendlysxw@163.com>
2023-06-09 18:03:08 +0800

    docs: link docs

--- packages/link/stories/App.tsx
+++ packages/link/stories/App.tsx
@@ -1,14 +1,17 @@
 import { PropsWithChildren } from "react";
 import { Us3rAuthWithRainbowkitProvider } from "@us3r-network/auth-with-rainbowkit";
+import { ProfileStateProvider } from "@us3r-network/profile";
 import { LinkStateProvider } from "@us3r-network/link";
 import { CERAMIC_HOST } from "./constants";
 
 export default function App({ children }: PropsWithChildren) {
   return (
     <Us3rAuthWithRainbowkitProvider>
-      <LinkStateProvider ceramicHost={CERAMIC_HOST}>
-        {children}
-      </LinkStateProvider>
+      <ProfileStateProvider ceramicHost={CERAMIC_HOST}>
+        <LinkStateProvider ceramicHost={CERAMIC_HOST}>
+          {children}
+        </LinkStateProvider>
+      </ProfileStateProvider>
     </Us3rAuthWithRainbowkitProvider>
   );
 }
