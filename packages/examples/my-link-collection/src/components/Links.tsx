c237d1050a35c25de8c6c709817052e3c41bcfba
bufan <bufan@hotmail.com>
2023-06-27 10:00:04 +0800

    STEP-6 add pagination to link list

--- packages/examples/my-link-collection/src/components/Links.tsx
+++ packages/examples/my-link-collection/src/components/Links.tsx
@@ -1,55 +1,143 @@
 import { S3LinkModel, Link } from '@us3r-network/data-model'
 import { useEffect, useState } from 'react'
-import { GridList, Item, Switch } from 'react-aria-components'
+import { Button, GridList, Item, Switch } from 'react-aria-components'
 import { CERAMIC_HOST } from '../constants'
 import { UserAvatar, UserName } from '@us3r-network/profile'
-import { FavorButton, VoteButton } from '@us3r-network/link'
+import { FavorButton } from '@us3r-network/link'
 import { useSession } from '@us3r-network/auth-with-rainbowkit'
+import type {
+  PageInfo,
+  Pagination,
+  ForwardPagination,
+  BackwardPagination
+} from '@ceramicnetwork/common'
 
 const s3LinkModel = new S3LinkModel(CERAMIC_HOST)
+const PAGE_SIZE = 1
+
+export enum PaginationKind {
+  FORWARD,
+  BACKWARD
+}
+
+// pagination type gateway
+export function parsePagination (query: Pagination): PaginationKind | undefined {
+  if ('first' in query && 'after' in query) {
+    console.log('forward')
+    return PaginationKind.FORWARD
+  } else if ('last' in query && 'before' in query) {
+    console.log('backward')
+    return PaginationKind.BACKWARD
+  }
+}
 
 export default function Links () {
   const [links, setLinks] = useState<Link[]>([])
   const [showFavorOnly, setShowFavorOnly] = useState<Boolean>(false)
+
+  const [queryParamsInfo, setQueryParamsInfo] = useState<Pagination>({
+    last: PAGE_SIZE
+  })
+  const [pageInfo, setPageInfo] = useState<PageInfo>()
   const session = useSession()
-  
+
   useEffect(() => {
     if (showFavorOnly) {
+      //personal links
       if (!session) return
       s3LinkModel.authComposeClient(session)
-      s3LinkModel.queryPersonalLinksDesc({ last: 20 }).then(res => {
-        const resLinks =
-          res.data?.viewer?.linkList?.edges
-            ?.filter(edge => !!edge?.node)
-            .map(edge => edge?.node) || []
-        setLinks(resLinks.reverse())
-      })
+      if (parsePagination(queryParamsInfo) === PaginationKind.FORWARD) {
+        //prev page
+        s3LinkModel
+          .queryPersonalLinks(queryParamsInfo as ForwardPagination)
+          .then(res => {
+            const resLinks =
+              res.data?.viewer?.linkList?.edges
+                ?.filter(edge => !!edge?.node)
+                .map(edge => edge?.node) || []
+            setLinks(resLinks.reverse())
+            setPageInfo(res.data?.viewer?.linkList?.pageInfo)
+          })
+      } else {
+        //next page
+        s3LinkModel
+          .queryPersonalLinksDesc(queryParamsInfo as BackwardPagination)
+          .then(res => {
+            const resLinks =
+              res.data?.viewer?.linkList?.edges
+                ?.filter(edge => !!edge?.node)
+                .map(edge => edge?.node) || []
+            setLinks(resLinks.reverse())
+            setPageInfo(res.data?.viewer?.linkList?.pageInfo)
+          })
+      }
     } else {
-      s3LinkModel.queryLinksDesc({ last: 20 }).then(res => {
-        const resLinks =
-          res.data?.linkIndex?.edges
-            ?.filter(edge => !!edge?.node)
-            .map(edge => edge?.node) || []
-        setLinks(resLinks.reverse())
-      })
+      //all links
+      if (parsePagination(queryParamsInfo) === PaginationKind.FORWARD) {
+        //prev page
+        s3LinkModel
+          .queryLinks(queryParamsInfo as ForwardPagination)
+          .then(res => {
+            const resLinks =
+              res.data?.linkIndex?.edges
+                ?.filter(edge => !!edge?.node)
+                .map(edge => edge?.node) || []
+            setLinks(resLinks.reverse())
+            setPageInfo(res.data?.linkIndex?.pageInfo)
+          })
+      } else {
+        //next page
+        s3LinkModel
+          .queryLinksDesc(queryParamsInfo as BackwardPagination)
+          .then(res => {
+            const resLinks =
+              res.data?.linkIndex?.edges
+                ?.filter(edge => !!edge?.node)
+                .map(edge => edge?.node) || []
+            setLinks(resLinks.reverse())
+            setPageInfo(res.data?.linkIndex?.pageInfo)
+          })
+      }
     }
-  }, [session, showFavorOnly])
+  }, [session, showFavorOnly, queryParamsInfo])
 
+  const prevPage = () => {
+    setQueryParamsInfo({ first: PAGE_SIZE, after: pageInfo?.endCursor })
+  }
+  const nextPage = () => {
+    setQueryParamsInfo({ last: PAGE_SIZE, before: pageInfo?.startCursor })
+  }
   return (
-    // <>
-    //   {links.map(link => {
-    //     return (
-    //       <p key={link.id}>
-    //         <a href={link?.url}>{link?.title}</a>
-    //         <span>{link?.createAt}</span>
-    //       </p>
-    //     )
-    //   })}
-    // </>
     <>
       <Switch onChange={setShowFavorOnly}>
-        {showFavorOnly? 'Show All Links': 'Show Favor Links Only'}
+        {showFavorOnly ? 'Show All Links' : 'Show Favor Links Only'}
       </Switch>
+      <>
+        {/* when query backward, hasNextPage is always false */}
+        <Button
+          isDisabled={
+            !(
+              pageInfo?.hasNextPage ||
+              parsePagination(queryParamsInfo) === PaginationKind.BACKWARD
+            )
+          }
+          onPress={prevPage}
+        >
+          Prev Page
+        </Button>
+        {/* when query forward, hasPreviousPage is always false */}
+        <Button
+          isDisabled={
+            !(
+              pageInfo?.hasPreviousPage ||
+              parsePagination(queryParamsInfo) === PaginationKind.FORWARD
+            )
+          }
+          onPress={nextPage}
+        >
+          Next Page
+        </Button>
+      </>
       <GridList aria-label='links' selectionMode='none'>
         {links.map(link => (
           <Item key={link?.id} textValue={link?.url}>
