5ce49fb9773e57f5f27a79619d39e4ce218d2f48
bufan <bufan@hotmail.com>
2023-06-29 14:35:32 +0800

    step-5 fix layout and styles

--- packages/examples/link-collections/src/App.tsx
+++ packages/examples/link-collections/src/App.tsx
@@ -1,17 +1,19 @@
-import { Us3rAuthWithRainbowkitProvider } from '@us3r-network/auth-with-rainbowkit';
-import AuthButton from './components/AuthButton';
-import Links from './components/Links';
-import './App.css';
-import CreateLink from './components/CreateLink';
+import { Us3rAuthWithRainbowkitProvider } from '@us3r-network/auth-with-rainbowkit'
+import AuthButton from './components/AuthButton'
+import Links from './components/Links'
+import './App.css'
+import CreateLink from './components/CreateLink'
 
-function App() {
+function App () {
   return (
     <Us3rAuthWithRainbowkitProvider>
-      <AuthButton />
-      <Links />
-      <CreateLink/>
+      <div className='app'>
+        <AuthButton />
+        <Links />
+        <CreateLink />
+      </div>
     </Us3rAuthWithRainbowkitProvider>
-  );
+  )
 }
 
 export default App
