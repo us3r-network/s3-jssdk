b4d05feccc185d1ebe9fb2484b65532627967703
bufan <bufan@hotmail.com>
2023-06-29 14:28:33 +0800

    step-3 create link

--- packages/examples/link-collections/src/App.tsx
+++ packages/examples/link-collections/src/App.tsx
@@ -2,12 +2,14 @@ import { Us3rAuthWithRainbowkitProvider } from '@us3r-network/auth-with-rainbowk
 import AuthButton from './components/AuthButton';
 import Links from './components/Links';
 import './App.css';
+import CreateLink from './components/CreateLink';
 
 function App() {
   return (
     <Us3rAuthWithRainbowkitProvider>
       <AuthButton />
       <Links />
+      <CreateLink/>
     </Us3rAuthWithRainbowkitProvider>
   );
 }
