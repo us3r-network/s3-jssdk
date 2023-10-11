8ab080c66b1bb1a3b62067e014c476b2403f78a8
shixuewen <friendlysxw@163.com>
2023-10-11 09:20:18 +0800

    feat: query biolink

--- packages/data-model/src/profile/index.ts
+++ packages/data-model/src/profile/index.ts
@@ -1,11 +1,12 @@
 import { RuntimeCompositeDefinition } from "@composedb/types";
-import type { CeramicApi } from "@ceramicnetwork/common";
-import { S3Model } from "../base";
+import type { CeramicApi, Page } from "@ceramicnetwork/common";
+import { Creator, S3Model } from "../base";
 
-import { definition as profileDefinition } from "./profile-runtime-composite";
+import { definition as profileDefinition } from "./profileV2-runtime-composite";
 
 export type WalletChainType = "EVM" | "SOLANA";
 export type Profile = {
+  id?: string;
   name?: string;
   avatar?: string;
   wallets?: Wallet[];
@@ -16,6 +17,28 @@ export type Wallet = {
   chain: WalletChainType;
   address: string;
   primary: boolean;
+};
+export type BioLink = {
+  id: string;
+  profileID: string;
+  creator: Creator;
+  platform?: string;
+  network?: string;
+  handle?: string;
+  data?: string;
+};
+export type BioLinkInput = {
+  profileID: string;
+  platform?: string;
+  network?: string;
+  handle?: string;
+  data?: string;
+};
+
+export type BioLinksEqualToFilters = {
+  platform?: string;
+  network?: string;
+  handle?: string;
 };
 
 export class S3ProfileModel extends S3Model {
@@ -163,6 +186,201 @@ export class S3ProfileModel extends S3Model {
       id: did,
     });
 
+    return res;
+  }
+
+  /**
+   * BioLink
+   */
+
+  public async createBioLink(input: BioLinkInput) {
+    const createMutation = `
+      mutation CreateBioLink($input: CreateBioLinkInput!) {
+        createBioLink(input: $input) {
+          document {
+            id
+          }
+        }
+      }
+    `;
+    const composeClient = this.composeClient;
+    const res = await composeClient.executeQuery<{
+      createBioLink: { document: { id: string } };
+    }>(createMutation, {
+      input: {
+        content: {
+          ...input,
+        },
+      },
+    });
+    return res;
+  }
+
+  public async updateBioLink(id: string, input: Partial<BioLinkInput>) {
+    const updateMutation = `
+      mutation UpdateBioLink($input: UpdateBioLinkInput!) {
+        updateBioLink(input: $input) {
+          document {
+            id
+          }
+        }
+      }
+    `;
+    const composeClient = this.composeClient;
+    const res = await composeClient.executeQuery<{
+      updateBioLink: { document: { id: string } };
+    }>(updateMutation, {
+      input: {
+        id,
+        content: {
+          ...input,
+        },
+      },
+    });
+    return res;
+  }
+
+  public async queryPersonalBioLinks({
+    first = 10,
+    after = "",
+  }: {
+    first: number;
+    after?: string;
+  }) {
+    const profileComposeClient = this.composeClient;
+    const profile = await profileComposeClient.executeQuery<{
+      viewer: {
+        bioLinkList: Page<BioLink>;
+      };
+    }>(`
+      query {
+        viewer {
+          bioLinkList(first: ${first}, after: "${after}") {
+            edges {
+              node {
+                data
+                handle
+                id
+                network
+                platform
+                profileID
+              }
+            }
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+              startCursor
+              endCursor
+            }
+          }
+        }
+      }
+    `);
+    return profile;
+  }
+
+  public async queryBioLinksWithDid(
+    did: string,
+    {
+      first = 10,
+      after = "",
+    }: {
+      first: number;
+      after?: string;
+    }
+  ) {
+    const query = `
+      query ($id: ID!) {
+        node(id: $id) {
+          ...on CeramicAccount {
+            id
+            bioLinkList(first: ${first}, after: "${after}") {
+              edges {
+                node {
+                  id
+                  network
+                  platform
+                  data
+                  handle
+                  profileID
+                }
+              }
+              pageInfo {
+                hasNextPage
+                hasPreviousPage
+                startCursor
+                endCursor
+              }
+            }
+          }
+        }
+      }
+    `;
+    const profileComposeClient = this.composeClient;
+    const res = await profileComposeClient.executeQuery<{
+      node: {
+        id: string;
+        bioLinkList: Page<BioLink>;
+      };
+    }>(query, {
+      id: did,
+    });
+
+    return res;
+  }
+
+  public async queryBioLinksWithEqualToFilters({
+    filters,
+    first = 10,
+    after = "",
+  }: {
+    filters: BioLinksEqualToFilters;
+    first?: number;
+    after?: string;
+  }) {
+    const query = `
+      query ($input: BioLinkFiltersInput!) {
+        bioLinkIndex(filters: $input, first: ${first}, after: "${after}") {
+          edges {
+            node {
+              id
+              profileID
+              creator {
+                id
+              }
+              platform
+              network
+              handle
+              data
+            }
+          }
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            endCursor
+          }
+        }
+      }
+    `;
+    let inputAnd = [];
+    for (const key in filters) {
+      inputAnd.push({
+        where: {
+          [key]: {
+            equalTo: filters[key as keyof BioLinksEqualToFilters],
+          },
+        },
+      });
+    }
+    const composeClient = this.composeClient;
+    const res = await composeClient.executeQuery<{
+      bioLinkIndex: Page<BioLink>;
+    }>(query, {
+      input: {
+        and: inputAnd,
+      },
+    });
     return res;
   }
 }
